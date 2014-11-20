2d4813933edc41cedf34d3355604245302064cd3
Andre Filipe Barro <abarro@gmail.com>
2014-11-20 13:32:43 -0200

    users complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,43 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def create
-    render plain: params[:user].inspect
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :cpf)
+    end
 end
