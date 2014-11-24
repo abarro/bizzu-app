618095bba5ce4ea599e3f8dd9c89e790875a8557
Andre Filipe Barro <abarro@gmail.com>
2014-11-24 18:08:55 -0200

    created form partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,6 +38,13 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    redirect_to users_path
+  end
+
   private
     def user_params
       params.require(:user).permit(:name, :cpf, :sales, :avatar)
