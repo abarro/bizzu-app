8d1203cc6ecd3fc34f5cba400f62306b3935d5af
Andre Filipe Barro <abarro@gmail.com>
2014-11-24 12:08:24 -0200

    add simple authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+  http_basic_authenticate_with name: "admin", password: "password", except: [:index, :show]
+
   def new
     @user = User.new
   end
