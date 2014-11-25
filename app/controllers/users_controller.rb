b99d5573c2af8031d3f99cc4383038d5f4037f9c
Andre Filipe Barro <abarro@gmail.com>
2014-11-25 11:00:33 -0200

    Sign Up page no longer asks for login credentials

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  http_basic_authenticate_with name: "admin", password: "password", except: [:index, :show]
+  http_basic_authenticate_with name: "admin", password: "password", except: [:index, :show, :new]
 
   def new
     @user = User.new
