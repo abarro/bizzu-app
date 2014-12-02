f41c8e447e7685392bdd43763fbe501b65d34ff2
Andre Filipe Barro <abarro@gmail.com>
2014-12-02 15:49:22 -0200

    Add sum of total sales

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,6 +29,7 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
+    @sales_total = User.sum(:sales)
   end
 
   def update
@@ -55,7 +56,7 @@ class UsersController < ApplicationController
 
     def logged_in_user
       unless logged_in?
-        flash[:danger] = "Please log in."
+        flash[:danger] = "Por favor faça login."
         redirect_to login_url
       end
     end
