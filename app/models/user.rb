75c7e247a4a969e628d62ac52e634e39288e0599
Andre Filipe Barro <abarro@gmail.com>
2014-12-03 17:39:23 -0200

    authenticate users by cpf

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,5 @@
 class User < ActiveRecord::Base
 
-  has_secure_password
-  before_save :encrypt_password
-  attr_accessor :password_salt, :password_hash
-
-  validates :password, confirmation: true
-  validates :password, presence: true, :on => :create
   validates :name, presence: true
   validates :email, presence: true
   validates :cpf, presence: true, :cpf => true
@@ -15,10 +9,4 @@ class User < ActiveRecord::Base
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "photo-default.png" 
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
-  def encrypt_password
-    if password.present?
-      self.password_salt = BCrypt::Engine.generate_salt
-      self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
-    end
-  end
 end
